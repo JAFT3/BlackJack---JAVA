2ae79943c9496613f6ee7becd25811d305fd8236
Jesus Antonio Fernandez de la Torre <fernandezdelatorrejesusantonio@gmail.com>
2024-11-12 18:29:51 -0600

    Subiendo las 2 clases main del programa.

---
+++ src/BlackJack/Main.java
@@ -0,0 +1,12 @@
+package BlackJack;
+
+import BlackJack.process.BlackJack;
+import BlackJack.ui.CLI;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BlackJack blackJack = new BlackJack();
+
+    }
+}
